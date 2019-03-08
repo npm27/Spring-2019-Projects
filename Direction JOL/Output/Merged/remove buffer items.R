2afcc5a478c9a674b2c86b34d2873dbb86637b82
Nicholas Maxwell <35810320+npm27@users.noreply.github.com>
2019-03-08 07:45:29 -0600

    script to remove buffer items

--- Direction JOL/Output/Merged/remove buffer items.R
+++ Direction JOL/Output/Merged/remove buffer items.R
@@ -2,24 +2,13 @@
 library(dplyr)
 
 dat = read.csv("output 3_7.csv")
-z = dat$cue_target == "Member - Group"
 
-grep("Hear", dat$cue_target)
-
-##this code removes rows containing a certain string in the designated column
-dat2 = filter(dat, !grepl("Hear", dat$cue_target))
-dat3 = filter(dat2, !grepl("Member", dat2$cue_target))
-
-##This will take forever...
-##is there a way to accomplish this with a loop?
-
-####Try a loop!####
 ##make list containing key words for removal
 exclude = c("Member", "Curtain", "Snow", "Haircut", "Shrimp",
             "Catcher", "Friendliness", "Geese", "Grab", "Trees",
-            "Hide", "Disintegrate", "Sheriff", "Essay", "Malt",
+            "Hide - Scare", "Disintegrate", "Sheriff", "Essay", "Malt",
             "Deciet", "Deaf", "Magician", "Contest", "Mind")
 
-for (word in exlcude){
-  
-}
+datx = filter(dat, !grepl(paste(exclude, collapse = "|"), dat$cue_target))
+
+#write.csv(datx, file = ('output 3_7 no buffers.csv'))
